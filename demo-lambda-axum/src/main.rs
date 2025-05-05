9f49fdd304309708edb0647ed47b60f924b947fd
Ernesto Menéndez <pyalec@gmail.com>
2025-05-05 13:48:11 -0600

    Add TraceLayer to log more details at Level::INFO

--- demo-lambda-axum/src/main.rs
+++ demo-lambda-axum/src/main.rs
@@ -4,11 +4,17 @@ mod templates;
 use axum::{Router, routing::get};
 use axum::response::Redirect;
 use lambda_http::{run, tracing, Error};
+use tracing::Level;
+use tower_http::trace::{DefaultOnRequest, DefaultOnResponse, TraceLayer};
 use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     tracing::init_default_subscriber();
+
+    let trace_layer = TraceLayer::new_for_http()
+        .on_request(DefaultOnRequest::new().level(Level::INFO))
+        .on_response(DefaultOnResponse::new().level(Level::INFO));
 
     let app = Router::new()
         .route("/", get(redirect_to_index))
@@ -21,7 +27,8 @@ async fn main() -> Result<(), Error> {
               Router::new()
                   .route("/index.html", get(templates::get_index))
         )
-        .nest_service("/static", ServeDir::new("static"));
+        .nest_service("/static", ServeDir::new("static"))
+        .layer(trace_layer);
     
     run(app).await
 }
