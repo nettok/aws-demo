bf1659e71b7e8f4dd0eaa2377a7afbcc9284a2ea
Ernesto Menéndez <pyalec@gmail.com>
2025-05-17 15:24:17 -0600

    be explicit about supported configuration run profiles

--- demo-lambda-axum/src/config.rs
+++ demo-lambda-axum/src/config.rs
@@ -1,12 +1,30 @@
 use crate::error::AppError;
 use config::Config;
 use serde::Deserialize;
+use std::str::FromStr;
+use strum::{Display, EnumString};
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct AppConfig {}
 
+#[derive(Clone, Display, EnumString)]
+enum RunProfile {
+    #[strum(serialize = "dev")]
+    Dev,
+
+    #[strum(serialize = "prod")]
+    Prod,
+}
+
 pub fn load_app_config() -> Result<AppConfig, AppError> {
-    let profile = std::env::var("CONFIG_PROFILE").unwrap_or_else(|_| "dev".to_owned());
+    let default_run_profile = RunProfile::Dev;
+
+    let profile = std::env::var("RUN_PROFILE")
+        .map(|env_profile| {
+            RunProfile::from_str(&env_profile).unwrap_or(default_run_profile.clone())
+        })
+        .unwrap_or(default_run_profile)
+        .to_string();
 
     let conf = Config::builder()
         .add_source(config::File::with_name("config/default"))
