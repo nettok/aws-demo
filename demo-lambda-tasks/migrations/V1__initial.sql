189d0b47a3a88f633d429548bdd56b6fe22dbce3
Ernesto Menéndez <pyalec@gmail.com>
2025-06-15 18:20:29 -0600

    add "entries" table to initial migration

--- demo-lambda-tasks/migrations/V1__initial.sql
+++ demo-lambda-tasks/migrations/V1__initial.sql
@@ -1,4 +1,16 @@
 create table users (
-    name varchar(255),
-    password varchar(255)
+    id uuid primary key,
+    name varchar(255) not null unique,
+    password varchar(255) not null,
+    created_at timestamp default current_timestamp
+);
+
+create table entries (
+    id uuid not null,       -- must be UUID v7 to preserve insertion order (use same timezone as "date" column for consistency)
+    user_id uuid not null,
+    date date not null,
+    content text not null,
+    created_at timestamp default current_timestamp,
+    primary key (user_id, date, id),
+    constraint fk_user foreign key (user_id) references users(id) on delete cascade
 );
